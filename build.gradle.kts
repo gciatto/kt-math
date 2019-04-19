c631530c7d4b4a8b70aef98cd93ea56df4233fa9
Giovanni <giovanni.ciatto@unibo.it>
2019-04-19 13:05:37 +0200

    maven central ok, need to setup dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,28 @@
+import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.dsl.KotlinJsCompile
 
 plugins {
     kotlin("multiplatform") version "1.3.21"
     id("maven-publish")
+    signing
+    id("org.jetbrains.dokka") version "0.9.18"
 }
 
 repositories {
     mavenCentral()
+    maven("https://dl.bintray.com/kotlin/dokka")
+    maven("https://jitpack.io")
 }
 
 group = "com.github.gciatto"
 version = "0.0.1"
+
+//val dokkaFatJar by configurations.creating
+//
+//dependencies {
+//    dokkaFatJar("org.jetbrains.dokka:dokka-gradle-plugin:0.9.16")
+//    dokkaFatJar("capital.scalable:spring-auto-restdocs-dokka-json:2.0.1")
+//}
 
 kotlin {
 
@@ -75,6 +87,56 @@ kotlin {
     }
 }
 
+tasks.withType<DokkaTask> {
+
+    outputDirectory = "$buildDir/javadoc"
+    jdkVersion = 8
+    reportUndocumented = false
+    outputFormat = "javadoc"
+
+    kotlinTasks {
+        listOf()
+    }
+}
+
+//tasks.getByName<DokkaTask>("dokka") {
+////    dependsOn("repackDokka")
+//
+//    outputFormat = "html"
+//    outputDirectory = "$buildDir/javadoc"
+////    dokkaFatJar = tasks.getByName<Jar>("repackDokka").outputs.files.singleFile
+//
+//    kotlinTasks {
+//        listOf()
+////        listOf("compileKotlinJvm", "compileKotlinMetadata")
+//    }
+//
+//    impliedPlatforms = mutableListOf("Common")
+//
+//    sourceRoot {
+//        // assuming there is only a single source dir...
+//        kotlin.sourceSets.commonMain {
+//            this@sourceRoot.path = kotlin.srcDirs.first().absolutePath
+//        }
+//        platforms = listOf("Common")
+//    }
+//    sourceRoot {
+//        // assuming there is only a single source dir...
+//        with(kotlin.sourceSets.get("jvmMain")) {
+//            this@sourceRoot.path = kotlin.srcDirs.first().absolutePath
+//        }
+//        platforms = listOf("JVM")
+//    }
+//}
+
+signing {
+    useGpgCmd()
+    sign(configurations.archives.get())
+    sign(publishing.publications)
+}
+
+// https://central.sonatype.org/pages/requirements.html
+// https://docs.gradle.org/current/userguide/signing_plugin.html
 publishing {
     publications.withType<MavenPublication> {
         groupId = rootProject.group.toString()
@@ -90,7 +152,40 @@ publishing {
                     url.set("https://openjdk.java.net/legal/gplv2+ce.html")
                 }
             }
+
+            developers {
+                developer {
+                    email.set("giovanni.ciatto@gmail.com")
+                    url.set("https://about.me/gciatto")
+                    organization.set("GitHub")
+                    organizationUrl.set("https://github.com")
+                }
+            }
+
+            scm {
+                connection.set("scm:git:git:///github.com/gciatto/kt-math.git")
+                developerConnection.set("scm:git:ssh://github.com:gciatto/kt-math.git")
+                url.set("https://github.com/gciatto/kt-math")
+            }
         }
 
     }
+
+    repositories {
+
+        val mavenRepoUrl = if (version.toString().contains("SNAPSHOT")) {
+            "https://oss.sonatype.org/content/repositories/snapshots/"
+        } else {
+            "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
+        }
+
+        maven(mavenRepoUrl) {
+            credentials {
+                username = rootProject.property("ossrhUsername").toString()
+                password = rootProject.property("ossrhPassword").toString()
+            }
+        }
+    }
+
+
 }
