fe4b866094936a004dc67d8667519975a444c9ff
Giovanni <giovanni.ciatto@unibo.it>
2021-01-14 10:30:24 +0100

    remove all io.* classes

--- src/commonMain/kotlin/io/gciatto/kt/math/RoundingModeFactory.kt
+++
@@ -1,5 +0,0 @@
-package io.gciatto.kt.math
-
-internal expect object RoundingModeFactory {
-    fun roundingModeOf(value: Int): RoundingMode
-}
--- src/jvmMain/kotlin/io/gciatto/kt/math/RoundingModeFactory.kt
+++
@@ -1,7 +0,0 @@
-package io.gciatto.kt.math
-
-internal actual object RoundingModeFactory {
-    actual fun roundingModeOf(value: Int): RoundingMode {
-        TODO()
-    }
-}
