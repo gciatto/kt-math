825cbb87856a2ce4d9c93ae19a798d360ac72a6b
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-06-19 17:48:56 +0200

    fix(build): disable allWarningAsErrors in buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     `kotlin-dsl`
+    `java-gradle-plugin`
+    alias(libs.plugins.kotlin.jvm)
 }
 
 repositories {
@@ -16,7 +18,8 @@ java {
 
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
     kotlinOptions {
-        allWarningsAsErrors = true
+        allWarningsAsErrors = false
+        languageVersion = "1.5"
         jvmTarget = javaVersion.toString()
     }
 }
