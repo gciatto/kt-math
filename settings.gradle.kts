b8422f53f5ad0a478dda82541cf4008fe2c8f5e7
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2022-06-23 11:24:21 +0200

    chore(build): add gradle enterprise plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,19 @@
+plugins {
+    id("com.gradle.enterprise") version "3.10.2"
+}
+
 rootProject.name = "kt-math"
 
 enableFeaturePreview("VERSION_CATALOGS")
 
 dependencyResolutionManagement {
 //    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        publishOnFailure()
+    }
 }
