9e94d38d9a7045542ef90d433969c56c527a5d76
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-03-07 14:26:10 +0100

    fix: remove version catalog preview

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,8 +4,6 @@ plugins {
 
 rootProject.name = "kt-math"
 
-enableFeaturePreview("VERSION_CATALOGS")
-
 dependencyResolutionManagement {
 //    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 }
