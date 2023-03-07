edd6a98932939a11a5fd35319138ccd0178ad2a1
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-03-07 15:41:00 +0100

    fix(build): reduce contraints on jvm version in buildSrc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,21 +9,6 @@ repositories {
     gradlePluginPortal()
 }
 
-val javaVersion = JavaVersion.current()
-
-java {
-    targetCompatibility = javaVersion
-    sourceCompatibility = javaVersion
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        allWarningsAsErrors = false
-        languageVersion = "1.5"
-        jvmTarget = javaVersion.toString()
-    }
-}
-
 dependencies {
     implementation(libs.kotlin.gradlePlugin)
     implementation(libs.npmPublish)
