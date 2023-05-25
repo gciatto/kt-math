31420f402882e93a5dbe19717549f59dd7a934a4
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-05-25 14:29:40 +0200

    fix(release): version numbering on NPM release

--- release.config.js
+++ release.config.js
@@ -1,9 +1,11 @@
 var staging = "-PstagingRepositoryId=${process.env.STAGING_REPO_ID}"
+var version = "-PforceVersion=${process.env.ENFORCE_VERSION}"
 
 var publishCmd = `
-./gradlew ${staging} releaseStagingRepositoryOnMavenCentral || exit 3
-./gradlew ${staging} publishJsPackageToNpmjsRegistry || exit 4
+./gradlew ${version} ${staging} releaseStagingRepositoryOnMavenCentral || exit 3
+./gradlew ${version} ${staging} publishJsPackageToNpmjsRegistry || exit 4
 `
+
 var config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
     [
