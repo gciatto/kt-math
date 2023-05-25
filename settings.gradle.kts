2300d87677e0a36cbaa4aef541295cfba0fadd12
Giovanni Ciatto <giovanni.ciatto@unibo.it>
2023-05-25 19:08:17 +0200

    fix(ci): show gradle scans in ci

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,11 +14,27 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 
+fun tag(format: String, vararg properties: String): String =
+    format.format(*properties.map { System.getProperty(it) ?: error("Missing property $it") }.toTypedArray())
+
+val ci = !System.getenv("CI").isNullOrBlank()
+val ciTag = if (ci) "CI" else "Local"
+val osTag = "OS: " + tag("%s (%s) v. %s", "os.name", "os.arch", "os.version")
+val jvmTag = "JVM: " + tag("%s v. %s", "java.vm.name", "java.vm.version")
+
 gradleEnterprise {
     buildScan {
         termsOfServiceUrl = "https://gradle.com/terms-of-service"
         termsOfServiceAgree = "yes"
+        tag(ciTag)
+        tag(osTag)
+        tag(jvmTag)
         publishOnFailure()
+        buildScanPublished {
+            if (ci) {
+                println("::error title=Gradle scan for ${osTag}, ${jvmTag}::$buildScanUri")
+            }
+        }
     }
 }
 
